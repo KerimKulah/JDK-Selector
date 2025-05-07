c0c117247250dbf41cf132270dcbf97d040208f4
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2025-05-07 23:54:42 +0300

    Update JDKSelectorWrapper.bat

--- JDKSelectorWrapper.bat
+++ JDKSelectorWrapper.bat
@@ -2,5 +2,5 @@
 :: .bat dosyasının bulunduğu dizini al
 set "SCRIPT_DIR=%~dp0"
 
-:: PowerShell'in tam yolunu kullanarak yönetici olarak başlat ve ExecutionPolicy'yi RemoteSigned yap
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy RemoteSigned -File \"%SCRIPT_DIR%JDKSelector.ps1\"' -Verb runAs"
+:: PowerShell'in tam yolunu kullanarak yönetici olarak başlat ve ExecutionPolicy'yi Unrestricted yap
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Unrestricted -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Unrestricted -File \"%SCRIPT_DIR%JDKSelector.ps1\"' -Verb runAs"
