6a7c9c453c899b54a08562ab32a5194fe3b9ba1e
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2025-05-08 17:23:05 +0300

    Update JDKSelectorWrapper.bat

--- JDKSelectorWrapper.bat
+++ JDKSelectorWrapper.bat
@@ -2,5 +2,5 @@
 :: .bat dosyasının bulunduğu dizini al
 set "SCRIPT_DIR=%~dp0"
 
-:: PowerShell'in tam yolunu kullanarak yönetici olarak başlat ve ExecutionPolicy'yi Unrestricted yap
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Unrestricted -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Unrestricted -File \"%SCRIPT_DIR%JDKSelector.ps1\"' -Verb runAs"
+:: Yönetici haklarıyla PowerShell başlat ve sadece bu işlemde ExecutionPolicy'yi Bypass yaparak script'i çalıştır
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"\"%SCRIPT_DIR%JDKSelector.ps1\"\"' -Verb runAs"
